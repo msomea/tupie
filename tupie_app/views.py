a1eee4c4e44969b94fa7832b5f0f52bc37481b07
msomea <msomearaphael@gmail.com>
2025-07-16 22:11:49 +0300

    Refactored navbar into reusable navbar.html and included in all templates

--- tupie_app/views.py
+++ tupie_app/views.py
@@ -4,3 +4,13 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     return render(request, 'tupie_app/home.html')
+
+def list_item(request):
+    return render(request, 'tupie_app/list_item.html')
+
+def about(request):
+    return render(request, 'tupie_app/about.html')
+
+def donation(request):
+    return render(request, 'tupie_app/donation.html')
+
